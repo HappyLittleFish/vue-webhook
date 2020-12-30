12352bac9bd60a1c966b1a9c3a5e2785723e6fd4
happylittlefish <happylittlefish@icloud.com>
2020-12-30 09:57:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const server = http.createServer(function(req,res){
+	console.log(req.method, req.url)
+	if (req.method === 'POST' && req.url === '/webhook') {
+		res.serHeader('Content-Type', 'application/json')
+		res.end(JSON.stringify({ok: true}))
+	} else {
+		res.end('Not Found')
+	}
+})
+
+server.listen(5000, () => {
+	console.log('webhook已经在5000端口启动')
+})
